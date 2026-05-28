d7b72b016d4695bc73f14a7344e3e8bbfcac631c
Mike Adams <mikethebos@icloud.com>
2026-05-28 10:24:50 -0600

    add numa aware allreduce to mpistubs

--- src/utilities/mpistubs.c
+++ src/utilities/mpistubs.c
@@ -918,16 +918,116 @@ HYPRE_Int hypre_MPI_Info_free( hypre_MPI_Info *info )
 
 #else
 
+// when updating NUM_MPIL_COMMS, update array initializations below
+#define NUM_MPIL_COMMS 16
+
+static MPI_Comm comms[NUM_MPIL_COMMS] = {MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL};
+static MPI_Comm leader_group_comms[NUM_MPIL_COMMS] = {MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL};
+static MPI_Comm leader_comms[NUM_MPIL_COMMS] = {MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL, MPI_COMM_NULL};
+static int num_procss[NUM_MPIL_COMMS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+static int num_nodess[NUM_MPIL_COMMS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+static int ppns[NUM_MPIL_COMMS] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
 HYPRE_Int
 hypre_MPI_Init( hypre_int   *argc,
                 char      ***argv )
 {
-   return (HYPRE_Int) MPI_Init(argc, argv);
+   HYPRE_Int out = (HYPRE_Int) MPI_Init(argc, argv);
+   int found = 0;   
+   comms[found] = MPI_COMM_WORLD;
+   
+   int rank, num_procs;
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+   MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+   num_procss[found] = num_procs;
+   
+   // Split global comm into local (per node) communicators
+   MPI_Comm local_comm;
+   MPI_Comm_split_type(MPI_COMM_WORLD,
+                        MPI_COMM_TYPE_SHARED,
+                        rank,
+                        MPI_INFO_NULL,
+                        &local_comm);
+
+   int local_rank, ppn;
+   MPI_Comm_rank(local_comm, &local_rank);
+   MPI_Comm_size(local_comm, &ppn);
+   ppns[found] = ppn;
+
+   // Split global comm into group (per local rank) communicators
+   MPI_Comm group_comm;
+   MPI_Comm_split(MPI_COMM_WORLD, local_rank, rank, &group_comm);
+
+   int node;
+   MPI_Comm_rank(group_comm, &node);
+
+   // Gather arrays for get_node, get_local, and get_global methods
+   // These arrays allow for these methods to work with any ordering
+   // No longer relying on SMP ordering of processes to nodes!
+   // Does rely on constant ppn
+   int *global_rank_to_local = (int*)malloc(num_procs * sizeof(int));
+   int *global_rank_to_node  = (int*)malloc(num_procs * sizeof(int));
+   MPI_Allgather(&local_rank,
+                  1,
+                  MPI_INT,
+                  global_rank_to_local,
+                  1,
+                  MPI_INT,
+                  MPI_COMM_WORLD);
+   MPI_Allgather(
+      &node, 1, MPI_INT, global_rank_to_node, 1, MPI_INT, MPI_COMM_WORLD);
+
+   int *ordered_global_ranks = (int*)malloc(num_procs * sizeof(int));
+   for (int i = 0; i < num_procs; i++)
+   {
+      int local                                       = global_rank_to_local[i];
+      int node                                        = global_rank_to_node[i];
+      ordered_global_ranks[node * ppn + local] = i;
+   }
+
+   // Set xcomm variables
+   int num_nodes = ((num_procs - 1) / ppn) + 1;
+   num_nodess[found] = num_nodes;
+   int rank_node = global_rank_to_node[rank];
+
+   // Convert to leader_comm (4 leaders per node)
+   int num_leaders = 4;
+   int procs_per_leader = ppn / num_leaders;
+   int leader = local_rank / procs_per_leader;
+   int leader_rank = local_rank % procs_per_leader;
+
+   MPI_Comm leader_comm;
+   MPI_Comm_split(
+      local_comm, local_rank / procs_per_leader, rank, &leader_comm);
+      
+   leader_comms[found] = leader_comm;
+      
+   MPI_Comm leader_group_comm;
+   MPI_Comm_split(MPI_COMM_WORLD, leader_rank, rank, &leader_group_comm);
+   
+   leader_group_comms[found] = leader_group_comm;
+   
+   free((void *)ordered_global_ranks);
+   free((void *)global_rank_to_local);
+   free((void *)global_rank_to_node);
+   MPI_Comm_free(&group_comm);
+   MPI_Comm_free(&local_comm);
+   
+   return out;
 }
 
 HYPRE_Int
 hypre_MPI_Finalize( void )
 {
+   for (int i = 0; i < NUM_MPIL_COMMS; ++i)
+   {
+      comms[i] = MPI_COMM_NULL;
+      if (leader_group_comms[i] != MPI_COMM_NULL) MPI_Comm_free(&(leader_group_comms[i]));
+      leader_group_comms[i] = MPI_COMM_NULL;
+      if (leader_comms[i] != MPI_COMM_NULL) MPI_Comm_free(&(leader_comms[i]));
+      leader_comms[i] = MPI_COMM_NULL;
+   }
+   
    return (HYPRE_Int) MPI_Finalize();
 }
 
@@ -1416,6 +1516,284 @@ hypre_MPI_Waitany( HYPRE_Int          count,
                                   &mpi_index, status);
    *index = (HYPRE_Int) mpi_index;
    return ierr;
+}
+
+int allreduce_dissemination_loc_core(
+                        const void* sendbuf,
+                        void* recvbuf,
+                        int count,
+                        MPI_Datatype datatype,
+                        MPI_Op op,
+                        MPI_Comm global_comm, 
+                        MPI_Comm group_comm,
+                        MPI_Comm local_comm,
+                        int tag)
+{
+    int type_size;
+    MPI_Type_size(datatype, &type_size);
+    
+    char *cpu_sendbuf = (char *)malloc(count*type_size);
+    char *cpu_recvbuf = (char *)malloc(count*type_size);
+
+    int rank, num_procs;
+    MPI_Comm_rank(global_comm, &rank);
+    MPI_Comm_size(global_comm, &num_procs);
+    
+    // to work on gpus and cpus
+    MPI_Sendrecv(sendbuf, count, datatype, rank, tag, cpu_sendbuf, count, datatype, rank, tag, global_comm, MPI_STATUS_IGNORE);
+
+    int local_rank, ppn;
+    MPI_Comm_rank(local_comm, &local_rank);
+    MPI_Comm_size(local_comm, &ppn);
+
+    int rank_node, num_nodes;
+    MPI_Comm_rank(group_comm, &rank_node);
+    MPI_Comm_size(group_comm, &num_nodes);
+
+    PMPI_Allreduce(cpu_sendbuf, cpu_recvbuf, count, datatype,
+            op, local_comm);
+
+    int pow_ppn_num_nodes = 1;
+    int base = ppn + 1;
+    while (pow_ppn_num_nodes * base <= num_nodes)
+        pow_ppn_num_nodes *= base;
+    int mult = num_nodes / pow_ppn_num_nodes;
+    int max_node = mult * pow_ppn_num_nodes;
+    int extra = num_nodes - max_node;
+
+    void *tmpbuf = (void *)malloc(type_size*count);
+
+    if (rank_node >= max_node)
+    {
+        int node = rank_node - max_node;
+        MPI_Send(cpu_recvbuf, count, datatype, node, tag, group_comm);
+        MPI_Recv(cpu_recvbuf, count, datatype, node, tag, group_comm,
+                MPI_STATUS_IGNORE);
+    }
+    else
+    {
+        if (rank_node < extra)
+        {
+            MPI_Recv(tmpbuf, count, datatype, max_node + rank_node, tag, group_comm,
+                   MPI_STATUS_IGNORE);
+            MPI_Reduce_local(tmpbuf, cpu_recvbuf, count, datatype, op);
+        }
+
+        for (int node_stride = 1; node_stride < max_node; node_stride *= (ppn+1))
+        {
+            int stride = node_stride * (local_rank+1);
+            if (stride < max_node)
+            {
+                int send_node = (rank_node - stride + max_node) % max_node;
+                int recv_node = (rank_node + stride) % max_node;
+                MPI_Sendrecv(cpu_recvbuf, count, datatype, send_node, tag,
+                        tmpbuf, count, datatype, recv_node, tag,
+                        group_comm, MPI_STATUS_IGNORE);
+            }
+            else
+            {
+                // Odd implementation to be portable to GPU
+                // Can have zerobuf be on CPU, regardless of
+                // where tmpbuf is located
+                char* zerobuf = (char*)malloc(type_size*count);
+                memset(zerobuf, 0, type_size*count);
+                MPI_Sendrecv(zerobuf, count, datatype, rank, tag,
+                        tmpbuf, count, datatype, rank, tag,
+                        global_comm, MPI_STATUS_IGNORE);
+                free(zerobuf);
+            }
+            PMPI_Allreduce(MPI_IN_PLACE, tmpbuf, count, datatype, op, local_comm);
+            MPI_Reduce_local(tmpbuf, cpu_recvbuf, count, datatype, op);
+        }
+
+        if (rank_node < extra)
+        {
+            MPI_Send(cpu_recvbuf, count, datatype, max_node + rank_node, tag, group_comm);
+        }
+    }
+
+    free((void*)tmpbuf);
+    
+    // to work on gpus and cpus
+    MPI_Sendrecv(cpu_recvbuf, count, datatype, rank, tag, recvbuf, count, datatype, rank, tag, global_comm, MPI_STATUS_IGNORE);
+    
+    free((void*)cpu_sendbuf);
+    free((void*)cpu_recvbuf);
+
+    return MPI_SUCCESS;
+}
+
+int numa_aware_allreduce(const void* sendbuf,
+                               void* recvbuf,
+                               int count,
+                               MPI_Datatype datatype,
+                               MPI_Op op,
+                               MPI_Comm comm)
+{
+   
+   int found = -1;
+   int i = -1;
+   while (i + 1 < NUM_MPIL_COMMS)
+   {
+      i++;
+      MPI_Comm i_comm = comms[i];
+      if (i_comm == MPI_COMM_NULL) break;
+      if (comm == MPI_COMM_WORLD && i_comm == MPI_COMM_WORLD)
+      {
+         found = i;
+         break;
+      }
+      int res = 0;
+      MPI_Comm_compare(comm, i_comm, &res);
+      if (res == MPI_IDENT)
+      {
+         found = i;
+         break;
+      }
+   }
+   
+   MPI_Comm leader_group_comm;
+   MPI_Comm leader_comm;
+   int num_procs;
+   int ppn;
+   int num_nodes;
+   
+   if (found >= 0)
+   {
+      leader_group_comm = leader_group_comms[found];
+      leader_comm = leader_comms[found];
+      num_procs = num_procss[found];
+      ppn = ppns[found];
+      num_nodes = num_nodess[found];
+   }
+   else
+   {
+      // skip alternative comms for now, only use NUMA-aware on MPI_COMM_WORLD setup in hypre_MPI_Init
+      return PMPI_Allreduce(sendbuf, recvbuf, count, datatype, op, comm);
+      /*
+      while (found + 1 < NUM_MPIL_COMMS)
+      {
+         found++;
+         if (comms[found] == MPI_COMM_NULL) break;
+      }
+      if (comms[found] != MPI_COMM_NULL)
+      {
+         comms[found] = MPI_COMM_NULL;
+         if (leader_group_comms[found] != MPI_COMM_NULL) MPI_Comm_free(&(leader_group_comms[found]));
+         leader_group_comms[found] = MPI_COMM_NULL;
+         if (leader_comms[found] != MPI_COMM_NULL) MPI_Comm_free(&(leader_comms[found]));
+         leader_comms[found] = MPI_COMM_NULL;
+      }
+      
+      comms[found] = comm;
+      
+      int rank;
+      MPI_Comm_rank(comm, &rank);
+      MPI_Comm_size(comm, &num_procs);
+      num_procss[found] = num_procs;
+      
+      // Split global comm into local (per node) communicators
+      MPI_Comm local_comm;
+      MPI_Comm_split_type(comm,
+                           MPI_COMM_TYPE_SHARED,
+                           rank,
+                           MPI_INFO_NULL,
+                           &local_comm);
+
+      int local_rank;
+      MPI_Comm_rank(local_comm, &local_rank);
+      MPI_Comm_size(local_comm, &ppn);
+      ppns[found] = ppn;
+
+      // Split global comm into group (per local rank) communicators
+      MPI_Comm group_comm;
+      MPI_Comm_split(comm, local_rank, rank, &group_comm);
+
+      int node;
+      MPI_Comm_rank(group_comm, &node);
+
+      // Gather arrays for get_node, get_local, and get_global methods
+      // These arrays allow for these methods to work with any ordering
+      // No longer relying on SMP ordering of processes to nodes!
+      // Does rely on constant ppn
+      int *global_rank_to_local = (int*)malloc(num_procs * sizeof(int));
+      int *global_rank_to_node  = (int*)malloc(num_procs * sizeof(int));
+      MPI_Allgather(&local_rank,
+                     1,
+                     MPI_INT,
+                     global_rank_to_local,
+                     1,
+                     MPI_INT,
+                     comm);
+      MPI_Allgather(
+         &node, 1, MPI_INT, global_rank_to_node, 1, MPI_INT, comm);
+
+      int *ordered_global_ranks = (int*)malloc(num_procs * sizeof(int));
+      for (int i = 0; i < num_procs; i++)
+      {
+         int local                                       = global_rank_to_local[i];
+         int node                                        = global_rank_to_node[i];
+         ordered_global_ranks[node * ppn + local] = i;
+      }
+
+      // Set xcomm variables
+      num_nodes = ((num_procs - 1) / ppn) + 1;
+      num_nodess[found] = num_nodes;
+      int rank_node = global_rank_to_node[rank];
+
+      // Convert to leader_comm (4 leaders per node)
+      int num_leaders = 4;
+      int procs_per_leader = ppn / num_leaders;
+      int leader = local_rank / procs_per_leader;
+      int leader_rank = local_rank % procs_per_leader;
+
+      MPI_Comm_split(
+         local_comm, local_rank / procs_per_leader, rank, &leader_comm);
+         
+      leader_comms[found] = leader_comm;
+
+      MPI_Comm_split(comm, leader_rank, rank, &leader_group_comm);
+      
+      leader_group_comms[found] = leader_group_comm;
+      
+      free((void *)ordered_global_ranks);
+      free((void *)global_rank_to_local);
+      free((void *)global_rank_to_node);
+      MPI_Comm_free(&group_comm);
+      MPI_Comm_free(&local_comm);
+      */
+   }
+   
+   if (num_nodes * ppn != num_procs)
+   {
+      return PMPI_Allreduce(sendbuf, recvbuf, count, datatype, op, comm);
+   }
+   else
+   {
+      return allreduce_dissemination_loc_core(
+                     sendbuf, recvbuf, count, datatype, op, 
+                     comm, leader_group_comm, 
+                     leader_comm, 77);
+   }
+}
+
+int MPI_Allreduce(const void* sendbuf,
+                        void* recvbuf,
+                        int count,
+                        MPI_Datatype datatype,
+                        MPI_Op op,
+                        MPI_Comm comm)
+{
+    if (count < 100)
+    {
+        return numa_aware_allreduce(sendbuf, recvbuf, count, datatype, op, comm);
+    }
+    else // Only use numa aware for small allreduces
+    {
+        return PMPI_Allreduce(sendbuf, recvbuf, count, datatype, op, comm);
+    }
+
+    return MPI_SUCCESS;
 }
 
 HYPRE_Int
