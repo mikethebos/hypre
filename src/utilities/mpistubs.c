13083b05ce748ca5f11cb82ff37be21138c8ac6e
Mike Adams <mikethebos@icloud.com>
2026-06-09 18:31:23 -0600

    stubs for MPIL Setup/Finish for api compatibility
    (cherry picked from commit 60619088db58bef6588288dedfdff1897bca364a)

--- src/utilities/mpistubs.c
+++ src/utilities/mpistubs.c
@@ -933,16 +933,34 @@ hypre_MPI_CheckCommMatrix( hypre_MPI_Comm   comm,
 
 #else
 
+HYPRE_Int 
+hypre_MPIL_Setup( void )
+{
+   return 0;
+}
+
 HYPRE_Int
 hypre_MPI_Init( hypre_int   *argc,
                 char      ***argv )
 {
-   return (HYPRE_Int) MPI_Init(argc, argv);
+   int out = (HYPRE_Int) MPI_Init(argc, argv);
+   
+   hypre_MPIL_Setup();
+   
+   return out;
+}
+
+HYPRE_Int
+hypre_MPIL_Finish( void )
+{
+   return 0;
 }
 
 HYPRE_Int
 hypre_MPI_Finalize( void )
 {
+   hypre_MPIL_Finish();
+   
    return (HYPRE_Int) MPI_Finalize();
 }
 
