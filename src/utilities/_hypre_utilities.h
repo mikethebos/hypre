14993b32855bd858d7fc1f4adbb1fa1d80098c64
Mike Adams <mikethebos@icloud.com>
2026-07-31 18:49:23 -0600

    only setup comms once, on demand enable/disable
    (cherry picked from commit 834d10c4cc8543a0f10ed2b6af88fb2537260a24)
    (cherry picked from commit 03731a59337d231af7d7755f65933d76068863e6)

--- src/utilities/_hypre_utilities.h
+++ src/utilities/_hypre_utilities.h
@@ -984,6 +984,8 @@ HYPRE_Int hypre_MPIL_Setup( void );
 HYPRE_Int hypre_MPI_Init( hypre_int *argc, char ***argv );
 HYPRE_Int hypre_MPIL_Finish( void );
 HYPRE_Int hypre_MPI_Finalize( void );
+HYPRE_Int hypre_MPIL_Enable( void );
+HYPRE_Int hypre_MPIL_Disable( void );
 HYPRE_Int hypre_MPI_Abort( hypre_MPI_Comm comm, HYPRE_Int errorcode );
 hypre_double hypre_MPI_Wtime( void );
 hypre_double hypre_MPI_Wtick( void );
